c0e7d80bb8b1ce2a17754f2ecfc96fad1298f7cf
Edison Arauco Valenzuela <e.araucov27@gmail.com>
2023-10-26 14:20:38 -0300

    actualizacion de html, css y js

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
-let ages = [];
-let totalAges = 0;
+// Declaración de variables y objetos
+const personas = [];
+let contadorPersonas = 0;
+let sumaEdades = 0;
 
-while (true) {
-    const name = prompt('Ingresa un nombre, si desea finalizar no ingrese ningun nombre');
-
-    if (!name) {
-        break;
-    }
-
-    const ageString = prompt('Ingresa la edad para ' + name + ':');
-    const age = parseInt(ageString, 10);
-
-    if (!isNaN(age)) {
-        ages.push(age);
-        totalAges += age;
-    } else {
-        alert('Edad no válida. Por favor, ingresa un número.');
-    }
+// Función para calcular el IMC
+function calcularIMC(peso, altura) {
+  const alturaEnMetros = altura / 100;
+  return peso / (alturaEnMetros * alturaEnMetros);
 }
 
-const numberOfPeople = ages.length;
-const averageAge = totalAges / numberOfPeople;
+// Captura de datos y cálculo del IMC
+while (true) {
+  const nombre = prompt("Ingrese el nombre (o escriba 'fin' para salir):");
+  if (nombre.toLowerCase() === "fin") {
+    break; // Salir del bucle si se ingresa "fin"
+  }
 
-alert('Número de personas ingresadas: ' + numberOfPeople);
-alert('Promedio de edad: ' + averageAge);
+  const apellido = prompt("Ingrese el apellido:");
+  const edad = parseInt(prompt("Ingrese la edad:"));
+  const peso = parseFloat(prompt("Ingrese el peso en kilogramos:"));
+  const altura = parseFloat(prompt("Ingrese la altura en centímetros:"));
+
+  const imc = calcularIMC(peso, altura);
+
+  personas.push({
+    nombre,
+    apellido,
+    edad,
+    imc,
+  });
+
+  contadorPersonas++;
+  sumaEdades += edad;
+}
+
+// Calcular el promedio de edad
+const promedioEdad = sumaEdades / contadorPersonas;
+
+// Mostrar los resultados
+console.log("Personas ingresadas:", contadorPersonas);
+console.log("Promedio de edad:", promedioEdad);
+
+// Mostrar información de cada persona
+personas.forEach((persona, index) => {
+  console.log(`Persona ${index + 1}:`);
+  console.log(`Nombre: ${persona.nombre} ${persona.apellido}`);
+  console.log(`Edad: ${persona.edad} años`);
+  console.log(`IMC: ${persona.imc.toFixed(2)}`); // Mostrar el IMC con dos decimales
+  console.log("-----------------------");
+});
