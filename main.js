304e2eea683e3fc4f0a9cb968153e4ca77ec8a42
Edison Arauco Valenzuela <e.araucov27@gmail.com>
2023-11-09 22:37:57 -0300

    se actualiza proyecto con json y dom

--- main.js
+++ main.js
@@ -1,51 +1,80 @@
-// Declaración de variables y objetos
-const personas = [];
-let contadorPersonas = 0;
-let sumaEdades = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const tablaPersonas = document.getElementById("tablaPersonas");
+  const contadorPersonas = document.getElementById("contadorPersonas");
+  const promedioEdad = document.getElementById("promedioEdad");
 
-// Función para calcular el IMC
-function calcularIMC(peso, altura) {
-  const alturaEnMetros = altura / 100;
-  return peso / (alturaEnMetros * alturaEnMetros);
-}
+  const nombreInput = document.getElementById("nombre");
+  const apellidoInput = document.getElementById("apellido");
+  const edadInput = document.getElementById("edad");
+  const pesoInput = document.getElementById("peso");
+  const alturaInput = document.getElementById("altura");
+  const agregarPersonaBtn = document.getElementById("agregarPersona");
 
-// Captura de datos y cálculo del IMC
-while (true) {
-  const nombre = prompt("Ingrese el nombre (o escriba 'fin' para salir):");
-  if (nombre.toLowerCase() === "fin") {
-    break; // Salir del bucle si se ingresa "fin"
+  const personas = [];
+
+  function calcularIMC(peso, altura) {
+      const alturaEnMetros = altura / 100;
+      return peso / (alturaEnMetros * alturaEnMetros);
   }
 
-  const apellido = prompt("Ingrese el apellido:");
-  const edad = parseInt(prompt("Ingrese la edad:"));
-  const peso = parseFloat(prompt("Ingrese el peso en kilogramos:"));
-  const altura = parseFloat(prompt("Ingrese la altura en centímetros:"));
+  function actualizarTabla() {
+      tablaPersonas.innerHTML = "";
+      let sumaEdades = 0;
 
-  const imc = calcularIMC(peso, altura);
+      for (let i = 0; i < personas.length; i++) {
+          const persona = personas[i];
 
-  personas.push({
-    nombre,
-    apellido,
-    edad,
-    imc,
+          const fila = document.createElement("tr");
+          const nombre = document.createElement("td");
+          const apellido = document.createElement("td");
+          const edad = document.createElement("td");
+          const imc = document.createElement("td");
+
+          nombre.textContent = persona.nombre;
+          apellido.textContent = persona.apellido;
+          edad.textContent = persona.edad;
+          imc.textContent = persona.imc.toFixed(2);
+
+          fila.appendChild(nombre);
+          fila.appendChild(apellido);
+          fila.appendChild(edad);
+          fila.appendChild(imc);
+
+          tablaPersonas.appendChild(fila);
+
+          sumaEdades += persona.edad;
+      }
+
+      contadorPersonas.textContent = personas.length;
+      promedioEdad.textContent = (personas.length > 0) ? (sumaEdades / personas.length).toFixed(2) : 0;
+  }
+
+  agregarPersonaBtn.addEventListener("click", function () {
+      const nombre = nombreInput.value;
+      const apellido = apellidoInput.value;
+      const edad = parseInt(edadInput.value);
+      const peso = parseFloat(pesoInput.value);
+      const altura = parseFloat(alturaInput.value);
+
+      if (nombre && apellido && !isNaN(edad) && !isNaN(peso) && !isNaN(altura)) {
+          const imc = calcularIMC(peso, altura);
+          const nuevaPersona = {
+              nombre,
+              apellido,
+              edad,
+              imc,
+          };
+
+          personas.push(nuevaPersona);
+          actualizarTabla();
+
+          nombreInput.value = "";
+          apellidoInput.value = "";
+          edadInput.value = "";
+          pesoInput.value = "";
+          alturaInput.value = "";
+      } else {
+          alert("Por favor, complete todos los campos con valores válidos.");
+      }
   });
-
-  contadorPersonas++;
-  sumaEdades += edad;
-}
-
-// Calcular el promedio de edad
-const promedioEdad = sumaEdades / contadorPersonas;
-
-// Mostrar los resultados
-console.log("Personas ingresadas:", contadorPersonas);
-console.log("Promedio de edad:", promedioEdad);
-
-// Mostrar información de cada persona
-personas.forEach((persona, index) => {
-  console.log(`Persona ${index + 1}:`);
-  console.log(`Nombre: ${persona.nombre} ${persona.apellido}`);
-  console.log(`Edad: ${persona.edad} años`);
-  console.log(`IMC: ${persona.imc.toFixed(2)}`); // Mostrar el IMC con dos decimales
-  console.log("-----------------------");
 });
