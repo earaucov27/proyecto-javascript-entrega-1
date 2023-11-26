eb1fdd3df79e05e3a14a0036bf067c0b85eb78ec
Edison Arauco Valenzuela <e.araucov27@gmail.com>
2023-11-26 20:48:54 -0300

    local storage
    Se agrega local storage para el mantenimiento de los datos de manera local

--- main.js
+++ main.js
@@ -1,80 +1,89 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tablaPersonas = document.getElementById("tablaPersonas");
-  const contadorPersonas = document.getElementById("contadorPersonas");
-  const promedioEdad = document.getElementById("promedioEdad");
+    const tablaPersonas = document.getElementById("tablaPersonas");
+    const contadorPersonas = document.getElementById("contadorPersonas");
+    const promedioEdad = document.getElementById("promedioEdad");
 
-  const nombreInput = document.getElementById("nombre");
-  const apellidoInput = document.getElementById("apellido");
-  const edadInput = document.getElementById("edad");
-  const pesoInput = document.getElementById("peso");
-  const alturaInput = document.getElementById("altura");
-  const agregarPersonaBtn = document.getElementById("agregarPersona");
+    const nombreInput = document.getElementById("nombre");
+    const apellidoInput = document.getElementById("apellido");
+    const edadInput = document.getElementById("edad");
+    const pesoInput = document.getElementById("peso");
+    const alturaInput = document.getElementById("altura");
+    const agregarPersonaBtn = document.getElementById("agregarPersona");
 
-  const personas = [];
+    let personas = [];
 
-  function calcularIMC(peso, altura) {
-      const alturaEnMetros = altura / 100;
-      return peso / (alturaEnMetros * alturaEnMetros);
-  }
+    function calcularIMC(peso, altura) {
+        const alturaEnMetros = altura / 100;
+        return peso / (alturaEnMetros * alturaEnMetros);
+    }
 
-  function actualizarTabla() {
-      tablaPersonas.innerHTML = "";
-      let sumaEdades = 0;
+    function actualizarTabla() {
+        tablaPersonas.innerHTML = "";
+        let sumaEdades = 0;
 
-      for (let i = 0; i < personas.length; i++) {
-          const persona = personas[i];
+        for (let i = 0; i < personas.length; i++) {
+            const persona = personas[i];
 
-          const fila = document.createElement("tr");
-          const nombre = document.createElement("td");
-          const apellido = document.createElement("td");
-          const edad = document.createElement("td");
-          const imc = document.createElement("td");
+            const fila = document.createElement("tr");
+            const nombre = document.createElement("td");
+            const apellido = document.createElement("td");
+            const edad = document.createElement("td");
+            const imc = document.createElement("td");
 
-          nombre.textContent = persona.nombre;
-          apellido.textContent = persona.apellido;
-          edad.textContent = persona.edad;
-          imc.textContent = persona.imc.toFixed(2);
+            nombre.textContent = persona.nombre;
+            apellido.textContent = persona.apellido;
+            edad.textContent = persona.edad;
+            imc.textContent = persona.imc.toFixed(2);
 
-          fila.appendChild(nombre);
-          fila.appendChild(apellido);
-          fila.appendChild(edad);
-          fila.appendChild(imc);
+            fila.appendChild(nombre);
+            fila.appendChild(apellido);
+            fila.appendChild(edad);
+            fila.appendChild(imc);
 
-          tablaPersonas.appendChild(fila);
+            tablaPersonas.appendChild(fila);
 
-          sumaEdades += persona.edad;
-      }
+            sumaEdades += persona.edad;
+        }
 
-      contadorPersonas.textContent = personas.length;
-      promedioEdad.textContent = (personas.length > 0) ? (sumaEdades / personas.length).toFixed(2) : 0;
-  }
+        contadorPersonas.textContent = personas.length;
+        promedioEdad.textContent = (personas.length > 0) ? (sumaEdades / personas.length).toFixed(2) : 0;
+    }
 
-  agregarPersonaBtn.addEventListener("click", function () {
-      const nombre = nombreInput.value;
-      const apellido = apellidoInput.value;
-      const edad = parseInt(edadInput.value);
-      const peso = parseFloat(pesoInput.value);
-      const altura = parseFloat(alturaInput.value);
+    agregarPersonaBtn.addEventListener("click", function () {
+        const nombre = nombreInput.value;
+        const apellido = apellidoInput.value;
+        const edad = parseInt(edadInput.value);
+        const peso = parseFloat(pesoInput.value);
+        const altura = parseFloat(alturaInput.value);
 
-      if (nombre && apellido && !isNaN(edad) && !isNaN(peso) && !isNaN(altura)) {
-          const imc = calcularIMC(peso, altura);
-          const nuevaPersona = {
-              nombre,
-              apellido,
-              edad,
-              imc,
-          };
+        if (nombre && apellido && !isNaN(edad) && !isNaN(peso) && !isNaN(altura)) {
+            const imc = calcularIMC(peso, altura);
+            const nuevaPersona = {
+                nombre,
+                apellido,
+                edad,
+                imc,
+            };
 
-          personas.push(nuevaPersona);
-          actualizarTabla();
+            personas.push(nuevaPersona);
+            localStorage.setItem("personas", JSON.stringify(personas));
+            actualizarTabla();
 
-          nombreInput.value = "";
-          apellidoInput.value = "";
-          edadInput.value = "";
-          pesoInput.value = "";
-          alturaInput.value = "";
-      } else {
-          alert("Por favor, complete todos los campos con valores válidos.");
-      }
-  });
+            nombreInput.value = "";
+            apellidoInput.value = "";
+            edadInput.value = "";
+            pesoInput.value = "";
+            alturaInput.value = "";
+        } else {
+            alert("Por favor, complete todos los campos con valores válidos.");
+        }
+    });
+
+    // Verificar si hay datos en el Local Storage al cargar la página
+    const datosGuardados = localStorage.getItem("personas");
+
+    if (datosGuardados) {
+        personas = JSON.parse(datosGuardados);
+        actualizarTabla();
+    }
 });
